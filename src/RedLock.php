351a2df00c0515d1d9861c49f193dcd8dd40cea7
Peter Scopes <peter.scopes@randomstorm.com>
2016-07-28 11:18:58 +0100

    Added namespace to class file and PHPDoc

--- src/RedLock.php
+++ src/RedLock.php
@@ -1,16 +1,55 @@
 <?php
 
+namespace RedLock;
+
+/**
+ * Class RedLock
+ *
+ * @package RedLock
+ * @author  Ronny Lopez <ronny@tangotree.io>
+ */
 class RedLock
 {
+    /**
+     * @var int Seconds to delay retrying
+     */
     private $retryDelay;
+
+    /**
+     * @var int Number lock attempt retries before giving up
+     */
     private $retryCount;
+
+    /**
+     * @var float Account for Redis expires precision
+     */
     private $clockDriftFactor = 0.01;
 
+    /**
+     * @var mixed
+     */
     private $quorum;
 
+    /**
+     * @var array[] Array of server information arrays: [host, port, timeout]
+     * @see \Redis::connect()
+     */
     private $servers = array();
+
+    /**
+     * @var array|\Redis[]
+     */
     private $instances = array();
 
+    /**
+     * RedLock constructor.
+     *
+     * @param array[] $servers    Each element should an array of host, port, timeout
+     * @param int     $retryDelay Seconds delay between retries
+     * @param int     $retryCount Number of times to retry
+     *
+     * @see \Redis::connect()
+     */
     function __construct(array $servers, $retryDelay = 200, $retryCount = 3)
     {
         $this->servers = $servers;
@@ -21,6 +60,12 @@ class RedLock
         $this->quorum  = min(count($servers), (count($servers) / 2 + 1));
     }
 
+    /**
+     * @param string $resource Unique identifier
+     * @param int    $ttl      Time to live
+     *
+     * @return array|bool
+     */
     public function lock($resource, $ttl)
     {
         $this->initInstances();
@@ -70,6 +115,10 @@ class RedLock
         return false;
     }
 
+    /**
+     * @param array $lock Array returned by RedLock::lock
+     * @see lock()
+     */
     public function unlock(array $lock)
     {
         $this->initInstances();
@@ -81,6 +130,9 @@ class RedLock
         }
     }
 
+    /**
+     * Initialise the Redis servers provided in the the constructor.
+     */
     private function initInstances()
     {
         if (empty($this->instances)) {
@@ -94,11 +146,26 @@ class RedLock
         }
     }
 
+    /**
+     * @param \Redis $instance Redis instance to attempt to lock
+     * @param string $resource Unique identifier
+     * @param string $token    Unique token
+     * @param int    $ttl      Time to live
+     *
+     * @return mixed
+     */
     private function lockInstance($instance, $resource, $token, $ttl)
     {
         return $instance->set($resource, $token, ['NX', 'PX' => $ttl]);
     }
 
+    /**
+     * @param \Redis $instance Redis instance to unlock
+     * @param string $resource Unique identifier
+     * @param string $token    Unique token
+     *
+     * @return mixed
+     */
     private function unlockInstance($instance, $resource, $token)
     {
         $script = '
